c7a753d8269341971d3761e45477d29080db37bb
Robert SIlaghi <silaghi@umich.edu>
2025-11-23 16:19:04 -0500

    Changed API data

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 DB_NAME = "asset_classes.db"
 
 # DO NOT REMOVE ENVIRONMENT VARIABLES
-SEC_API_KEY = os.environ.get("SEC_API_KEY", "cd987be4425e8c3064ce4d602f913b3edc27a1af91be9396fbc377055fc2e0de")
-STOCKDATA_API_KEY = os.environ.get("STOCKDATA_API_KEY", "MRexJ4p6r1G4c0u1wpB2ESUlZUg0qAUp7x9hrHJB")
+SEC_API_KEY = os.environ.get("SEC_API_KEY", "9ac33dd00ac48350c86f810689a689c6206eaa53abeef44838ea9dafdb62d675")
+STOCKDATA_API_KEY = os.environ.get("STOCKDATA_API_KEY", "yK3BNhS1Mzce5lqaHCgNGHkSfkBch8i4yo4KBkPE")
 FRED_API_KEY = os.environ.get("FRED_API_KEY", "6471ba9502c9cad924b13c056bf84aae")
 
 SEC_BASE_URL = "https://api.sec-api.io"
