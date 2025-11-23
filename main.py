25d5b103af71506fd44f7cee153694b02cc0ef5a
Phu Le <phule@umich.edu>
2025-11-23 17:02:29 -0500

    Add personal information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# Your names: Robert Silaghi 
-# Your student ids: 87125192 
-# Your emails: silaghi@umich.edu
+# Your names: Robert Silaghi, Phu Le
+# Your student ids: 87125192, 10566445
+# Your emails: silaghi@umich.edu, phule@umich.edu
 # Who or what you worked with on this homework (including generative AI like ChatGPT): ChatGPT
 # If you worked with generative AI also add a statement for how you used it.  
 
