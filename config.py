cdcec5fdcdbe41e40df035d4cf6e1c8c1fd274e8
Robert SIlaghi <silaghi@umich.edu>
2025-11-24 12:45:12 -0500

    For Sharing

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 DB_NAME = "asset_classes.db"
 
 # DO NOT REMOVE ENVIRONMENT VARIABLES :D
-SEC_API_KEY = os.environ.get("SEC_API_KEY", "9ac33dd00ac48350c86f810689a689c6206eaa53abeef44838ea9dafdb62d675")
-STOCKDATA_API_KEY = os.environ.get("STOCKDATA_API_KEY", "yK3BNhS1Mzce5lqaHCgNGHkSfkBch8i4yo4KBkPE")
-FRED_API_KEY = os.environ.get("FRED_API_KEY", "6471ba9502c9cad924b13c056bf84aae")
+SEC_API_KEY = os.environ.get("SEC_API_KEY", "INSERT HERE")
+STOCKDATA_API_KEY = os.environ.get("STOCKDATA_API_KEY", "INSERT HERE")
+FRED_API_KEY = os.environ.get("FRED_API_KEY", "INSERT HERE")
 
 SEC_BASE_URL = "https://api.sec-api.io"
-STOCKDATA_BASE_URL = "https://api.stockdata.org/v1"
+STOCKDATA_BASE_URL = "https://api.stockdata.org/v1"   #https://www.stockdata.org/
 FRED_BASE_URL = "https://api.stlouisfed.org/fred"
--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-# Your names: Robert Silaghi, Phu Le
-# Your student ids: 87125192, 10566445
-# Your emails: silaghi@umich.edu, phule@umich.edu
-# Who or what you worked with on this homework (including generative AI like ChatGPT): ChatGPT
-# If you worked with generative AI also add a statement for how you used it.  
-# We used ChatGPT for debugging all of our files other than config.py. We also used it to create our database structure in db.py. We also used it to make sure our variables are unique. 
-# We also used it to understand packages like datetime and delta. We also used it for some SQL commands like insert or ignore. We also used it for help for limiting our API call. 
-# We also used it for methods like .isoformat(). We also used it to give us print statements to use in debugging. Lastly, we used it for date related methods like .strftime().
-
-
 from db import create_tables
 from db import create_tables
 from pipeline import load_sec_data, load_and_store_stock_returns, load_interest_rate_data
